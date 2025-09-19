f75dc6a703d12b21ec215bb195fcaef539041782
totoogg <goncharov.vova2014@gmail.com>
2025-09-19 15:24:34 +0300

    feat: add test for section

--- frontend/src/app/components/pages/movie/section/section.component.spec.ts
+++ frontend/src/app/components/pages/movie/section/section.component.spec.ts
@@ -1,23 +1,48 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import type { ComponentFixture } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 import { SectionComponent } from './section.component';
+import { NavigationService } from '../services/navigation.service';
+import { ElementRef } from '@angular/core';
 
 describe('SectionComponent', () => {
-  let component: SectionComponent;
   let fixture: ComponentFixture<SectionComponent>;
+  let navServiceMock: jasmine.SpyObj<NavigationService>;
 
   beforeEach(async () => {
+    navServiceMock = jasmine.createSpyObj<NavigationService>(
+      'NavigationService',
+      ['registerSection', 'unregisterSection']
+    );
+
     await TestBed.configureTestingModule({
-      imports: [SectionComponent]
-    })
-    .compileComponents();
+      imports: [SectionComponent],
+      providers: [
+        { provide: NavigationService, useValue: navServiceMock },
+        {
+          provide: ElementRef,
+          useValue: new ElementRef(document.createElement('section')),
+        },
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(SectionComponent);
-    component = fixture.componentInstance;
+
+    fixture.componentRef.setInput('id', 'test-id');
+    fixture.componentRef.setInput('title', 'Test Title');
+
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('registerSection', () => {
+    expect(navServiceMock.registerSection).toHaveBeenCalledWith(
+      'test-id',
+      'Test Title',
+      jasmine.any(HTMLElement)
+    );
+  });
+
+  it('unregisterSection', () => {
+    fixture.destroy();
+    expect(navServiceMock.unregisterSection).toHaveBeenCalledWith('test-id');
   });
 });
